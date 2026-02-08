0c1af1720c09022a06b4f38b66e5e8842dafb3d4
youngsheldonstaff-sketch <youngsheldonstaff@gmail.com>
2026-02-08 08:59:59 +0300

    tupleprodhomework

---
+++ tupleprodhomework.py
@@ -0,0 +1,13 @@
+tup1 = (4, 2, 5, 8, -1, 9)
+tup2 = (4, 8, 8, 1, 3, 4)
+
+product1 = 1
+for num in tup1:
+    product1 *= num
+
+product2 = 1
+for num in tup2:
+    product2 *= num
+
+print("Product of tup1:", product1)
+print("Product of tup2:", product2)
